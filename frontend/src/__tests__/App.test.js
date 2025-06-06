16aa7867540aa139a1a2fa8975c764192a67c790
Amrehab22 <amreehab45@gmail.com>
2025-06-06 15:41:06 +0300

    frontend

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -3,6 +3,6 @@ import App from '../App';
 
 test('renders Task Manager heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Task Manager/i);
   expect(linkElement).toBeInTheDocument();
 });
